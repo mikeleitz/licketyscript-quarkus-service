920531c9faa7377a20a233d685278b3cf342edff
leitz <leitz@mikeleitz.com>
2020-10-28 21:41:59 -0400

    Try to implement native graal.

--- .space.kts
+++ .space.kts
@@ -1,5 +1,5 @@
 job("build and publish") {
-    container("oracle/graalvm-ce:20.2.0") {
+    container("oracle/graalvm-ce:20.2.0-java11") {
         kotlinScript { api ->
             api.gradlew("build -Dquarkus.package.type=native")
             try {

2761267a95e0ca36079a22ce10aacc19805a2e38
leitz <leitz@mikeleitz.com>
2020-10-28 22:39:29 -0400

    Try to implement quarkus uberjar.

--- .space.kts
+++ .space.kts
@@ -1,11 +1,7 @@
 job("build and publish") {
     container("oracle/graalvm-ce:20.2.0-java11") {
         kotlinScript { api ->
-            shellScript {
-                content = "gu install native-image"
-            }
-
-            api.gradlew("build", "-Dquarkus.package.type=native")
+            api.gradlew("quarkusBuild", "--uber-jar")
             try {
                 api.gradlew("publish")
             } catch (ex: Exception) {

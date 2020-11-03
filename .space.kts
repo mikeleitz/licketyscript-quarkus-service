f88b0b3d67449fa0e983cb97b876cd242e02faaa
leitz <leitz@mikeleitz.com>
2020-11-03 00:01:16 -0500

    Iterate to generate final docker image.

--- .space.kts
+++ .space.kts
@@ -1,20 +1,10 @@
 job("build and publish") {
     container("oracle/graalvm-ce:20.2.0-java11") {
-        sequential {
-
-            shellScript {
-                content = "./gradlew build quarkusBuild --uber-jar publish"
-            }
-
-            shellScript {
-                content = "ls -la build/libs/licketyscript-quarkus-service-1.0-SNAPSHOT.jar"
-            }
-            shellScript {
-                content = "mkdir -p $mountDir/share"
-            }
-            shellScript {
-                content = "cp build/libs/licketyscript-quarkus-service-1.0-SNAPSHOT.jar $mountDir/share/licketyscript-quarkus-service-1.0-SNAPSHOT.jar"
-            }
+        shellScript {
+            content = "./gradlew build quarkusBuild --uber-jar publish"
+            content = "ls -la build/libs/licketyscript-quarkus-service-1.0-SNAPSHOT.jar"
+            content = "mkdir -p $mountDir/share"
+            content = "cp build/libs/licketyscript-quarkus-service-1.0-SNAPSHOT.jar $mountDir/share/licketyscript-quarkus-service-1.0-SNAPSHOT.jar"
         }
     }
 }

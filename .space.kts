86273c3729abd9ce58ed5d69b0ee9509864204c5
leitz <leitz@mikeleitz.com>
2020-11-02 23:42:10 -0500

    Iterate to generate final docker image.

--- .space.kts
+++ .space.kts
@@ -1,30 +1,23 @@
 job("build and publish") {
     container("oracle/graalvm-ce:20.2.0-java11") {
-        kotlinScript { api ->
-            api.gradlew("quarkusBuild", "--uber-jar")
-            try {
-                api.gradlew("publish")
-            } catch (ex: Exception) {
-                println("Publishing failed")
-            }
-
-            api.fileShare().put(File("$mountDir/share/licketyscript-quarkus-service-1.0-SNAPSHOT.jar"), "build/libs/licketyscript-quarkus-service-1.0-SNAPSHOT.jar")
-        }
-    }
-
-    container("ubuntu") {
         shellScript {
-            content = "ls -la $mountDir/share/licketyscript-quarkus-service-1.0-SNAPSHOT.jar"
+            content = "gradle build quarkusBuild --uber-jar publish"
         }
     }
-/*
-    docker {
-        build {
-            context = "/mnt/space/work"
-            file = "src/main/docker/Dockerfile.jvm"
-        }
-        push("leadtechnologist.registry.jetbrains.space/p/lsc/leadtechnologist-containers/licketyscript-quarkus-service") {
-            tag = "latest"
-        }
-    }*/
+
+    /*
+    container("ubuntu") {
+          shellScript {
+              content = "ls -la $mountDir/share/licketyscript-quarkus-service-1.0-SNAPSHOT.jar"
+          }
+      }
+      docker {
+          build {
+              context = "/mnt/space/work"
+              file = "src/main/docker/Dockerfile.jvm"
+          }
+          push("leadtechnologist.registry.jetbrains.space/p/lsc/leadtechnologist-containers/licketyscript-quarkus-service") {
+              tag = "latest"
+          }
+      }*/
 }

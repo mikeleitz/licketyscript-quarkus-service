0ec4a47669e0517b0af0d317ac1e705972d661c8
leitz <leitz@mikeleitz.com>
2020-11-04 22:06:28 -0500

    Try to get the final docker image to build.

--- .space.kts
+++ .space.kts
@@ -1,5 +1,5 @@
 job("build and publish") {
-    container("oracle/graalvm-ce:20.2.0-java11") {
+    container("docker") {
         shellScript {
             content = """
                     ./gradlew build -Dquarkus.package.type=native

5f618b05a27c0f87a476b63e3edb961e70a21f2a
leitz <leitz@mikeleitz.com>
2020-11-04 22:30:31 -0500

    Try to get the final docker image to build.

--- .space.kts
+++ .space.kts
@@ -2,8 +2,8 @@ job("build and publish") {
     container("docker") {
         shellScript {
             content = """
-                    apk add openjdk11             
-                    ./gradlew build -Dquarkus.package.type=native
+                    apk add openjdk11
+                    ./gradlew build -Dquarkus.package.type=native -Dquarkus.native.container-build=true
                     pwd
                     ls -lahrt
                     mkdir -p /mnt/space/work/build/libs

5907babf294d99646f83718855d876457f6134af
leitz <leitz@mikeleitz.com>
2020-11-04 23:17:23 -0500

    Try to get the final docker image to build.

--- .space.kts
+++ .space.kts
@@ -1,20 +1,23 @@
 job("build and publish") {
-    container("docker") {
+    container("oracle/graalvm-ce:20.2.0-java11") {
         shellScript {
             content = """
-                    apk add openjdk11
-                    ./gradlew build -Dquarkus.package.type=native -Dquarkus.native.container-build=true -Dquarkus.native.builder-image="leadtechnologist.registry.jetbrains.space/p/lsc/leadtechnologist-containers/ubi-quarkus-native-image:20.2.0-java11"
-                    pwd
-                    ls -lahrt
+                    ./gradlew build quarkusBuild --uber-jar publish
                     mkdir -p /mnt/space/work/build/libs
-                    ls -lahrt /mnt/space/work/build/*-runner 
+                    cp build/libs/licketyscript-quarkus-service-1.0-SNAPSHOT.jar /mnt/space/work/build/libs
                 """
         }
     }
     docker {
+        beforeBuildScript {
+            content = """
+                pwd
+                ls -lahrt
+                """
+        }
         build {
-            context = "/mnt/space/work"
-            file = "/mnt/space/work/src/main/docker/Dockerfile.native"
+            context = ""
+            file = "src/main/docker/Dockerfile.jvm"
         }
         push("leadtechnologist.registry.jetbrains.space/p/lsc/leadtechnologist-containers/licketyscript-quarkus-service") {
             tag = "latest"

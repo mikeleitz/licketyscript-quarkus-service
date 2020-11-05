1fee66a60093f630cad3f4ac0eebc4e1ac93eafe
leitz <leitz@mikeleitz.com>
2020-11-04 23:58:30 -0500

    Try to get the final docker image to build.

--- .space.kts
+++ .space.kts
@@ -16,7 +16,10 @@ job("build and publish") {
                 ls -lahrt src/main/docker/Dockerfile.jvm
                 ls -lahrt /mnt/
                 ls -lahrt /mnt/space/
+                ls -lahrt /mnt/space/share
                 ls -lahrt /mnt/space/work
+                mkdir -p build/libs
+                mv /mnt/space/share/licketyscript-quarkus-service-1.0-SNAPSHOT.jar build/libs
                 """
         }
         build {
